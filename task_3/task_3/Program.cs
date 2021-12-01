493cce39dbe7f97f7f23768860bf36de062b113d
velisalmanli1 <valivs@code.edu.az>
2021-12-02 01:15:16 +0400

    All Tasks Have Been Uploaded

---
+++ task_3/task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "cavid@code.edu.az";
+            string str2 = "12345";
+
+
+            Console.WriteLine("Enter an email...");
+            str1 = Console.ReadLine();
+
+            Console.WriteLine("Enter password...");
+            str2 = Console.ReadLine();
+
+            if (str1 = "cavid@code.edu.az", str2 = "12345")
+                     { Console.WriteLine("Welcome to your email"); }
+
+            else
+            { Console.WriteLine("Wrong email or password"); }
+
+
+
+
+
+        }
+    }
+}
